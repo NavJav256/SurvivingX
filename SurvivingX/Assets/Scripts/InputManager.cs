c05f7955ed0efcd9fc7ae03f24e9a452664ef23a
NavJav256 <55883312+NavJav256@users.noreply.github.com>
2021-11-05 17:01:06 +0000

    Animations attempt
    Tried importing some animations for the character and scripting them to work, script seems to work but player doesn't fully animate, can fix later

--- SurvivingX/Assets/Scripts/InputManager.cs
+++ SurvivingX/Assets/Scripts/InputManager.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     PlayerControls playerControls;
+    AnimatorManager animManager;
 
     [SerializeField]
     Vector2 movementInput;
+    float moveAmount;
     public float verticalInput;
     public float horizontalInput;
+
+    private void Awake()
+    {
+        animManager = GetComponent<AnimatorManager>();
+    }
 
     private void OnEnable()
     {
@@ -33,6 +40,8 @@ public class InputManager : MonoBehaviour
     {
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animManager.UpdateAnimValues(0, moveAmount);
     }
 
     public void HandleAllInputs()
