fe590a76e2fc1aad514c2069c0e92ad02791ecae
Sebastian Maliwiecki <60139438+SebastianMaliwiecki@users.noreply.github.com>
2021-11-10 19:24:31 +0000

    NavMesh and enemy NPC's added
    - Added NavMesh to map
    - Added Enemy NPC
    - Enemy NPC chases player around the map ONLY IF they enter their radius

---
+++ SurvivingX/Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform target;
+    NavMeshAgent agent;
+    public float lookRadius = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
+        }
+    }
+
+    // Point towards the player
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
