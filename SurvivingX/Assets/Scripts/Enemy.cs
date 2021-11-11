6239a4e5ec4c43527f0a01a12c0598d55a2ac44a
Sebastian Maliwiecki <60139438+SebastianMaliwiecki@users.noreply.github.com>
2021-11-11 01:17:44 +0000

    Enemy controller updated
    NPCs now move around randomly across the map, if player walks near them they will start to chase the player unless they run outside of their range.

--- SurvivingX/Assets/Scripts/Enemy.cs
+++ SurvivingX/Assets/Scripts/Enemy.cs
@@ -5,39 +5,56 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-
     [SerializeField]
     Transform target;
     NavMeshAgent agent;
     public float lookRadius = 10f;
+    public float moveTimer;
+    public float changeDirectionTimeLimit = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        changeWanderingDirection();
     }
 
     // Update is called once per frame
     void Update()
     {
         float distance = Vector3.Distance(target.position, transform.position);
-
+        
         if(distance <= lookRadius)
         {
             agent.SetDestination(target.position);
             if (distance <= agent.stoppingDistance)
             {
-                FaceTarget();
+                FaceTowardsTarget();
             }
         }
+
+        if (moveTimer >= changeDirectionTimeLimit)
+        {
+            moveTimer = 0;
+            changeWanderingDirection();
+        }
+
+        moveTimer += Time.deltaTime;
     }
 
     // Point towards the player
-    void FaceTarget()
+    void FaceTowardsTarget()
     {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void changeWanderingDirection()
+    {
+        float newXDirection = Random.Range(1, 500);
+        float newYDirection = Random.Range(1, 500);
+        agent.SetDestination(new Vector3(newXDirection, gameObject.transform.position.y, newYDirection));
     }
 
     private void OnDrawGizmosSelected()
