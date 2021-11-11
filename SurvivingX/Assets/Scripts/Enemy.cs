c02c2f15bc31ed33b45cc274d69f5a125b194c77
Sebastian Maliwiecki <60139438+SebastianMaliwiecki@users.noreply.github.com>
2021-11-11 14:06:13 +0000

    Update to Enemy.cs
    Comments added and new damage radius to accommodate damage dealt by NPCs

--- SurvivingX/Assets/Scripts/Enemy.cs
+++ SurvivingX/Assets/Scripts/Enemy.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,9 +6,11 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     Transform target;
     NavMeshAgent agent;
-    public float lookRadius = 10f;
-    public float moveTimer;
-    public float changeDirectionTimeLimit = 5f;
+    float moveTimer;
+    float changeDirectionTimeLimit = 5f;
+
+    public float lookRadius = 15f;
+    public float damageRadius = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,34 +22,51 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Distance from the player to agent (enemy)
         float distance = Vector3.Distance(target.position, transform.position);
-        
-        if(distance <= lookRadius)
+
+        if (distance <= lookRadius)
         {
             agent.SetDestination(target.position);
             if (distance <= agent.stoppingDistance)
             {
-                FaceTowardsTarget();
+                faceTarget();
+                // Deal damage to the player if within damage radius of 3
+                if (distance <= damageRadius)
+                {
+                    Debug.Log("I am dealing damage");
+                }
             }
         }
 
+        // Every 5 seconds change the direction the NPC travels
         if (moveTimer >= changeDirectionTimeLimit)
         {
             moveTimer = 0;
             changeWanderingDirection();
         }
 
+        // Increase timer by 1 second
         moveTimer += Time.deltaTime;
     }
 
+    // Draw radius
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+        Gizmos.DrawWireSphere(transform.position, damageRadius);
+    }
+
     // Point towards the player
-    void FaceTowardsTarget()
+    void faceTarget()
     {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 
+    // Change random direction
     void changeWanderingDirection()
     {
         float newXDirection = Random.Range(1, 500);
@@ -57,9 +74,4 @@ public class Enemy : MonoBehaviour
         agent.SetDestination(new Vector3(newXDirection, gameObject.transform.position.y, newYDirection));
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
-    }
 }
