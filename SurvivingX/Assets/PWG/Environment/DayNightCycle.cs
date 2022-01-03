cca031ad9cd10937e8e582c368dc95f37df5319a
Sebastian Maliwiecki <smaliwiecki@outlook.com>
2022-01-03 15:03:15 +0000

    Day and night cycle added
    Lighting colours still need adjusting otherwise it works fine

---
+++ SurvivingX/Assets/PWG/Environment/DayNightCycle.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+
+    [Range(0f, 1f)]
+    public float time;
+    public float dayLength; // In seconds
+    public float startDayTime = 0.4f; // Morning sun
+    private float timeRate;
+    public Vector3 noon; // Rotation of the sun
+
+    [Header("Sun settings")]
+    public Light lightOfSun;
+    public Gradient sunGradientColour;
+    public AnimationCurve sunLightIntensity;
+
+    [Header("Moon settings")]
+    public Light lightOfMoon;
+    public Gradient moonGradientColour;
+    public AnimationCurve moonLightIntensity;
+
+    [Header("Other Lighting options")]
+    public AnimationCurve lightingIntensityMultiplier;
+    public AnimationCurve reflectionIntensityMultiplier;
+
+    void Start()
+    {
+        timeRate = 1f / dayLength;
+        time = startDayTime;
+    }
+
+    void Update()
+    {
+        time += timeRate * Time.deltaTime;
+
+        if (time >= 1f)
+        {
+            time = 0f;
+        }
+
+        // Light rotation
+        lightOfSun.transform.eulerAngles = (time - 0.25f) * noon * 4f;
+        lightOfMoon.transform.eulerAngles = (time - 0.75f) * noon * 4f;
+
+        //Light intensity
+        lightOfSun.intensity = sunLightIntensity.Evaluate(time);
+        lightOfMoon.intensity = moonLightIntensity.Evaluate(time);
+
+        // Change colours
+        lightOfSun.color = sunGradientColour.Evaluate(time);
+        lightOfMoon.color = moonGradientColour.Evaluate(time);
+
+        // enabling lights
+        if(lightOfSun.intensity == 0 && lightOfSun.gameObject.activeInHierarchy)
+        {
+            lightOfSun.gameObject.SetActive(false);
+        }
+        else if(lightOfSun.intensity > 0 && !lightOfSun.gameObject.activeInHierarchy)
+        {
+            lightOfSun.gameObject.SetActive(true);
+        }
+
+        if (lightOfMoon.intensity == 0 && lightOfMoon.gameObject.activeInHierarchy)
+        {
+            lightOfMoon.gameObject.SetActive(false);
+        }
+        else if (lightOfMoon.intensity > 0 && !lightOfMoon.gameObject.activeInHierarchy)
+        {
+            lightOfMoon.gameObject.SetActive(true);
+        }
+
+        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+        //RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
+    }
+
+}
