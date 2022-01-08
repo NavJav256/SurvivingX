2379381e70b1bcf00880be988805c400389eed81
NavJav256 <ec19075@qmul.ac.uk>
2022-01-08 15:25:15 +0000

    skyboxes update

--- SurvivingX/Assets/PWG/Environment/DayNightCycle.cs
+++ SurvivingX/Assets/PWG/Environment/DayNightCycle.cs
@@ -11,9 +11,13 @@ public class DayNightCycle : MonoBehaviour
     public float startDayTime = 0.4f; // Morning sun
     private float timeRate;
     public Vector3 noon; // Rotation of the sun
-    public GameObject targetLight;
+
+
     public GameObject targetMainCamera;
     public Material[] skys;
+    public bool isCycle;
+
+
 
     [Header("Sun settings")]
     public Light lightOfSun;
@@ -31,7 +35,6 @@ public class DayNightCycle : MonoBehaviour
 
     void Awake() 
     {
-    //    targetLight = GameObject.FindGameObjectsWithTag("Light");
         targetMainCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
@@ -66,27 +69,37 @@ public class DayNightCycle : MonoBehaviour
         if(lightOfSun.intensity == 0 && lightOfSun.gameObject.activeInHierarchy)
         {
             lightOfSun.gameObject.SetActive(false);
-            targetMainCamera.GetComponent<Skybox>().material = skys[1];
+            //targetMainCamera.GetComponent<Skybox>().material = skys[1];
         }
         else if(lightOfSun.intensity > 0 && !lightOfSun.gameObject.activeInHierarchy)
         {
             lightOfSun.gameObject.SetActive(true);
-            targetMainCamera.GetComponent<Skybox>().material = skys[0];
+            //targetMainCamera.GetComponent<Skybox>().material = skys[0];
         }
 
         if (lightOfMoon.intensity == 0 && lightOfMoon.gameObject.activeInHierarchy)
         {
             lightOfMoon.gameObject.SetActive(false);
-            targetMainCamera.GetComponent<Skybox>().material = skys[0];
+            //targetMainCamera.GetComponent<Skybox>().material = skys[0];
         }
         else if (lightOfMoon.intensity > 0 && !lightOfMoon.gameObject.activeInHierarchy)
         {
             lightOfMoon.gameObject.SetActive(true);
-            targetMainCamera.GetComponent<Skybox>().material = skys[1];
+            //targetMainCamera.GetComponent<Skybox>().material = skys[1];
         }
+
+        ChangeCycle();
 
         RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
         //RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
+    }
+
+    void ChangeCycle()
+    {
+        // from 0.15 to 0.65, from 0.65 to 0.8, from 0.8 to 0.15
+        if (time >= 0.15f && time <= 0.65f) targetMainCamera.GetComponent<Skybox>().material = skys[0];
+        if (time > 0.65f && time <= 0.8f) targetMainCamera.GetComponent<Skybox>().material = skys[1];
+        if (time > 0.8f || time < 0.15f) targetMainCamera.GetComponent<Skybox>().material = skys[2];
     }
 
 }
