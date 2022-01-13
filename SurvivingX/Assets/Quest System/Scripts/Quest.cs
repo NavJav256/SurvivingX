aebed886de8e174eebb47f1d437cf4684980fb7c
NavJav256 <ec19075@qmul.ac.uk>
2022-01-13 19:48:47 +0000

    quest update

---
+++ SurvivingX/Assets/Quest System/Scripts/Quest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest
+{
+    public bool isActive;
+    public string title;
+    public string description;
+    public GameObject reward;
+    public int rewardAmount;
+}
